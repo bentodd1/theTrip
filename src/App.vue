03c53132942ea220111c9bfeb22219d7a29dccc1
Ben Todd <btodd@multiservice.com>
2018-07-18 22:44:52 -0500

    Changed to a quasar app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <div id="app">
-     <main>
-    <Scene />
-    </main>
+  <div id="q-app">
+    <router-view />
   </div>
 </template>
 
 <script>
-import Scene from './components/Scene.vue';
-
 export default {
-  name: 'app',
-  components: {
-    Scene,
-  },
-};
+  name: 'App'
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
